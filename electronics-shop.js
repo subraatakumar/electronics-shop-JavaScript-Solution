958278d9242d0a0d1ce28d8172189bd4e46f9a87
Subrat Kumar Das <72245773+subratsir@users.noreply.github.com>
2021-11-06 18:42:11 +0530

    Update electronics-shop.js

--- electronics-shop.js
+++ electronics-shop.js
@@ -29,13 +29,22 @@ function getMoneySpent(keyboards, drives, b) {
     /*
      * Write your code here.
      */
+    // let max = -1;
+    // for(let i = 0; i< keyboards.length; i++){
+    //     for(let j=0; j< drives.length; j++ ){
+    //         if(keyboards[i]+drives[j] <= b && keyboards[i]+drives[j] > max){
+    //             max = keyboards[i]+drives[j];
+    //         }
+    //     }
+    // }
     let max = -1;
     keyboards.forEach(keyboard => {
-        drives.forEach(drive =>{
-            if(((keyboard+drive) <= b) && ((keyboard+drive) > max))
-                max = keyboard + drive;
-        })
-    })
+        drives.forEach(drive => {
+            if(keyboard+drive <=b && keyboard+drive > max){
+                max = keyboard+drive;
+            }
+        });
+    });
     return max;
 }
 
